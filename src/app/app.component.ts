97015fb1142dd58f2051b2723b394568b29846af
wael moustafa elsaid <en.waelmoustafa@outlook.com>
2020-06-03 22:39:49 +0200

    Add Core Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AL-Nakhell Management';
+
+  constructor(private http: HttpClient) {
+    
+  }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe(
+      (response: any) => {
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  
 }
